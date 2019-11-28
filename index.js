fa668c9a40392fb29694b2458a089eb812b7f1e3
Mark Oosterveld <moosterveld@shuterstock.com>
2019-11-28 18:25:24 -0500

    output the id of the deployment

--- index.js
+++ index.js
@@ -1,52 +1,48 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const isEmpty = value => value === null || value.length === 0;
+const isNotEmpty = value => value !== null && value.length > 0;
 
 async function run() {
   try {
-    // const contextPayload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${contextPayload}`);
-
-    const token = core.getInput('token', { required: true });
+    const token = core.getInput("token", { required: true });
     const ref = core.getInput("ref");
-    const task = core.getInput('task');
-    const payload = core.getInput('payload');
-    const environment = core.getInput('environment');
-    const description = core.getInput('description');
+    const task = core.getInput("task");
+    const payload = core.getInput("payload");
+    const environment = core.getInput("environment");
+    const description = core.getInput("description");
+
+    const [owner, repo] = process.env["GITHUB_REPOSITORY"].split("/");
 
     console.log({ token, ref, task, payload, environment, description });
+    console.log({ owner, repo });
 
     const octokit = new github.GitHub(token);
 
-    const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/');
-
-    const request = { owner, repo,
+    const request = {
+      owner,
+      repo,
       required_contexts: [],
-      ref: isEmpty(ref) ? process.env['GITHUB_REF'] : ref,
-      task: isEmpty(task) ? undefined : task,
-      payload: isEmpty(payload) ? undefined: payload,
-      environment: isEmpty(environment) ? undefined: environment,
-      description: isEmpty(description) ? undefined: description
+      ...{ ref: isNotEmpty(ref) ? ref : process.env["GITHUB_REF"] },
+      ...(isNotEmpty(task) && { task }),
+      ...(isNotEmpty(payload) && { payload }),
+      ...(isNotEmpty(environment) && { environment }),
+      ...(isNotEmpty(description) && { description })
     };
 
-    console.log({request})
-
+    console.log({ request });
 
     // const ref = github.context.payload.ref;
-    // const ref = process.env['GITHUB_REF'];
-    console.log({ owner, repo, ref });
-
-    // console.log('env', process.env);
 
     // https://octokit.github.io/rest.js/
-    const foo = await octokit.repos.createDeployment(request);
-    console.log('foo', foo)
+    const {
+      data: { id }
+    } = await octokit.repos.createDeployment(request);
+    console.log("id", id);
 
-    core.setOutput('id', 999);
-  }
-  catch (error) {
-    console.log('error', error)
+    core.setOutput("id", id);
+  } catch (error) {
+    console.log("error", error);
     core.setFailed(error.message);
   }
 }
